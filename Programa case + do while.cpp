4112ecb71755ac7a1cf5b66263f4b4aba63786d2
lopaspi89 <102775975+lopaspi89@users.noreply.github.com>
2024-03-01 11:35:51 -0500

    Add files via upload

---
+++ Programa case + do while.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main()
+{
+int num;
+
+    do{
+        std::cout << "ingrese numero no mayor a 2 cifras" << std::endl;
+        std::cin >> num;
+        
+        switch (num){
+            case 45:
+            
+    
+             std::cout<< "ganaste" <<std::endl; 
+           
+            break;
+            
+            case 95:
+            
+				std::cout<< "perdiste" <<std::endl;
+	
+			break;	
+			
+			default:
+				std::cout<< "numero no gana ni pierde" <<std::endl;
+        }
+        
+    }while (num < 99);
+    
+  
+    
+    
+    
+    
+
+
+    return 0;
+}
+
